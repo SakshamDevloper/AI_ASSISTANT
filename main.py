072d22f283733325ac7e4659a70f97b3647c2d8d
Saksham Sethi <sakshamsethi353@gmail.com>
2026-03-04 16:03:11 +0530

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from assistant.speech import take_command
 from assistant.speak import speak
 from assistant.commands import execute_command
+from assistant.config import WAKE_WORD
 
 def run_assistant():
-    speak("Athena is ready.")
+
+    speak("Athena is ready")
 
     while True:
+
         command = take_command()
 
         if command:
-            if "athena" in command:
-                execute_command(command)
+
+            if WAKE_WORD in command:
+
+                status = execute_command(command)
+
+                if status == False:
+                    break
+
 
 if __name__ == "__main__":
     run_assistant()
